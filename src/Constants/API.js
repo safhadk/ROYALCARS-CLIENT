238684424db9a411d482046f1a583305a21cb510
safhadk <safadkothikkal0880@gmail.com>
2023-04-28 14:16:06 +0530

    first commit

---
+++ src/Constants/API.js
@@ -0,0 +1,4 @@
+export const userAPI = "http://localhost:5000/";
+export const adminAPI = "http://localhost:5000/admin/";
+export const ownerAPI = "http://localhost:5000/owner/";
+
---
+++ src/Pages/Clients/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../Components/clients/Header/Header";
+import Banner from "../../Components/clients/Banner/Banner";
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Banner/>
+        </div>
+    );
+}
+
+export default Home;
